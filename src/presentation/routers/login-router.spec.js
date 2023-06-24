b10c96c75dbbf49b4a292e69f63a5225d9d69d76
root <root@pop-os.localdomain>
2023-06-24 14:56:02 -0300

    feat: validando se existe httpRequest

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,10 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    if (!httpRequest) return { statusCode: 500 }
+
+    const { email, password } = httpRequest.body
+
+    if (!email || !password) {
       return {
         statusCode: 400
       }
@@ -23,7 +27,7 @@ describe('Login Router', () => {
 })
 
 describe('Login Router', () => {
-  test('Shoube return 400 if no email is provided', () => {
+  test('Shoube return 400 if no password is provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
@@ -33,5 +37,13 @@ describe('Login Router', () => {
 
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+})
+
+describe('Login Router', () => {
+  test('Shoube return 500 if httpRequest is provided', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
