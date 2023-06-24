35d8df644ce0fbeed6bb281b8eec7e2ad370f324
root <root@pop-os.localdomain>
2023-06-24 14:09:43 -0300

    feat: configuirado ambiente para commit e test padronizados

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.export = {
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  testEviroment: 'node'
+}
