31978a9f210023945115f9a7ec84ca78264d9f1e
Guilherme GRO. dos Reis Olinto <guilhermeolinto@gac.local>
2023-06-26 10:38:04 -0300

    feat: refatorando LoginRouter jest

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,40 +1,46 @@
-const LoginRouter = require('./login-router');
-const MissingParamError = require('../helpers/missing-error');
+const LoginRouter = require('./login-router')
+const MissingParamError = require('../helpers/missing-error')
+
+const makeSut = () => {
+  return new LoginRouter()
+}
 
 describe('Login Router', () => {
   test('Shoube return 400 if no email is provided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
       }
     }
 
-    const httpResponse = sut.route(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError('email'));
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
-})
 
-describe('Login Router', () => {
   test('Shoube return 400 if no password is provided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@gmail.com'
       }
     }
 
-    const httpResponse = sut.route(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError('password'));
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
-})
 
-describe('Login Router', () => {
   test('Shoube return 500 if httpRequest is provided', () => {
-    const sut = new LoginRouter()
-    const httpResponse = sut.route();
-    expect(httpResponse.statusCode).toBe(500);
+    const sut = makeSut()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Shoube return 500 if httpRequest.body is null', () => {
+    const sut = makeSut()
+    const httpResponse = sut.route({})
+    expect(httpResponse.statusCode).toEqual(500)
   })
 })
