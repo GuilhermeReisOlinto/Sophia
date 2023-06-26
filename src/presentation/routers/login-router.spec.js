dc5ba0353453215b5e0121cf799af1c2b4a8ae9d
Guilherme GRO. dos Reis Olinto <guilhermeolinto@gac.local>
2023-06-26 09:25:22 -0300

    feat: customizado errors de logins routers

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,14 +1,38 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest) return { statusCode: 500 }
+    if (!httpRequest) return HttpResponse.serverError();
 
     const { email, password } = httpRequest.body
 
-    if (!email || !password) {
-      return {
-        statusCode: 400
-      }
+    if (!email) {
+      return HttpResponse.badRequest('email');
     }
+    if (!password) {
+      return HttpResponse.badRequest('password');
+    }
+  }
+}
+
+class HttpResponse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+    }
+  };
+
+  static serverError () {
+    return {
+      statusCode: 500,
+      body: new MissingParamError()
+    }
+  }
+} 
+
+class MissingParamError extends Error {
+  constructor (paramName) {
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
   }
 }
 
@@ -21,8 +45,9 @@ describe('Login Router', () => {
       }
     }
 
-    const httpResponse = sut.route(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
   })
 })
 
@@ -35,15 +60,16 @@ describe('Login Router', () => {
       }
     }
 
-    const httpResponse = sut.route(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('password'));
   })
 })
 
 describe('Login Router', () => {
   test('Shoube return 500 if httpRequest is provided', () => {
     const sut = new LoginRouter()
-    const httpResponse = sut.route()
-    expect(httpResponse.statusCode).toBe(500)
+    const httpResponse = sut.route();
+    expect(httpResponse.statusCode).toBe(500);
   })
 })
